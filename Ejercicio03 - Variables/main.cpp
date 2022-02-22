39c24c35ba7a5298a7d563d16203113cb953eec2
Crisjra <98718225+Crisjra@users.noreply.github.com>
2022-02-22 09:56:10 -0600

    Add files via upload

---
+++ Ejercicio03 - Variables/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char const *argv[]) 
+
+{
+	int numeroEntero;
+	numeroEntero = 15;
+	
+	cout << " Entero: " << numeroEntero << endl;;
+	
+	numeroEntero = 18;
+	
+	cout << " Entero modificado: " << numeroEntero;
+	
+	return 0;
+}
